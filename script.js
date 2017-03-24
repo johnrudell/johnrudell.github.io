fb1af0f74bb1667e9ae3e00dd8675efad7d8893a
John Rudell <johnjrudell@gmail.com>
2017-03-24 18:49:44 -0400

    fixed header hover grey color and condensed js

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $(".navbar-custom").css('background-color', 'white');
-          $(".navbar-custom .navbar-nav li a").css('color', '#403D3C');
+          $(".navbar-custom").css({'background-color': 'white', 'border-bottom': '1px solid black'});
           $(".navbar-custom .navbar-nav li a").css('background-color', 'white');
           $(".navbar-custom .container .navbar-header .navbar-brand").html('John Rudell');
-          $(".navbar-custom").css('border-bottom', '1px solid black');
        } else {
-          $('.navbar-custom').css('background-color', 'transparent');
-          $(".navbar-custom .navbar-nav li a").css('color', '#403D3C');
+          $('.navbar-custom').css({'background-color': 'transparent', 'border-bottom': ''});
           $(".navbar-custom .navbar-nav li a").css('background-color', 'transparent');
           $(".navbar-custom .container .navbar-header .navbar-brand").html('');
-          $(".navbar-custom").css('border-bottom', '');
        }
    });
     }
